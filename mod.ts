b395af4a8a8f8d42e71c2a093c8ef62bee231d09
chiefportable <kwamea236@protonmail.com>
2024-08-21 01:49:02 +0000

    Add: personal details added to page

--- mod.ts
+++ mod.ts
@@ -3,23 +3,24 @@ import plugins from "./plugins.ts";
 import "lume/types.ts";
 
 export default function () {
-  return (site: Lume.Site) => {
-    // Configure the site
-    site.use(plugins());
+    return (site: Lume.Site) => {
+        // Configure the site
+        site.use(plugins());
 
-    // Add remote files
-    const files = [
-      "_includes/css/header.css",
-      "_includes/css/link.css",
-      "_includes/layouts/base.vto",
-      "index.yml",
-      "styles.css",
-      "favicon.svg",
-      "avatar.jpg",
-    ];
+        // Add remote files
+        const files = [
+            "_includes/css/header.css",
+            "_includes/css/link.css",
+            "_includes/layouts/base.vto",
+            "index.yml",
+            "styles.css",
+            "favicon.svg",
+            "avatar.jpg",
+            "sankofa.jpg",
+        ];
 
-    for (const file of files) {
-      site.remoteFile(file, import.meta.resolve(`./src/${file}`));
-    }
-  };
+        for (const file of files) {
+            site.remoteFile(file, import.meta.resolve(`./src/${file}`));
+        }
+    };
 }
